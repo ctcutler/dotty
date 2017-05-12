efd6cc13a282b7bf3721653f37c77e2af592a578
ctcutler <ctcutler@gmail.com>
2017-05-12 18:37:01 -0400

    retrieve tasks and stories

--- dotty/asana.py
+++ dotty/asana.py
@@ -4,12 +4,23 @@ import asana
 
 from dotty import board
 
+def task_history(stories):
+    return [story for story in stories]
+
 class AsanaBoard(board.Board):
     """ Asana implementation of Board abstract base class. """
-    def __init__(self, token):
+    def __init__(self, token, board_id):
         self.token = token
+        self.board_id = board_id
 
     def load(self):
         client = asana.Client.access_token(self.token)
-        me = client.users.me()
-        print(json.dumps(me))
+        for task in client.tasks.find_by_project(self.board_id):
+            # skip sections
+            if task['name'].endswith(':'):
+                continue
+            print()
+            print(task['name'])
+            stories = client.tasks.stories(task['id'])
+            history = task_history(stories)
+            print(json.dumps(history))

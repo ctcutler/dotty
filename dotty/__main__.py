6bd436eca0b0745e515111971722b28cad1cdff1
ctcutler <ctcutler@gmail.com>
2017-05-12 18:36:34 -0400

    complain about missing config file; abstract out board class handling

--- dotty/__main__.py
+++ dotty/__main__.py
@@ -7,19 +7,23 @@ if len(sys.argv) != 2:
   print("usage: python -m dotty config_file")
   sys.exit(-1)
 
-config_file = sys.argv[1]
+config_fn = sys.argv[1]
 config = configparser.ConfigParser()
-config.read(config_file)
+with open(config_fn) as config_file:
+    config.read_file(config_file)
 
 boards = []
 for section_name in config.sections():
     section = config[section_name]
     board_type = section['type']
+
     if board_type == 'asana':
-        board = asana.AsanaBoard(section['token'])
-        boards.append(board)
+        board_class = asana.AsanaBoard
     else:
         print('Ignoring unknown board type: {}'.format(board_type))
+
+    board = board_class(section['token'], section['id'])
+    boards.append(board)
 
 for board in boards:
     board.load()

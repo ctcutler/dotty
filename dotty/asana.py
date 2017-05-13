162c859c84094c3714fbfd8c362882102956cf7a
ctcutler <ctcutler@gmail.com>
2017-05-13 19:11:17 -0400

    dot calculation with tests

--- dotty/asana.py
+++ dotty/asana.py
@@ -1,6 +1,7 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 import json
 import re
+import time
 
 import asana
 
@@ -8,11 +9,14 @@ from dotty import board
 
 ADD_RE = re.compile(r'^added to (?P<board>.+)$')
 MOVE_RE = re.compile(r'^moved from (?P<from_column>.+) to (?P<column>.+) \((?P<board>.+)\)$')
+DAY = 24 * 60 * 60
+WEEK = 7 * DAY
+WEEKLY_DOTS = 5 * DAY
+DAILY_DOTS = DAY
 
 def iso8601_to_epoch(iso_string):
     dt = datetime.strptime(iso_string, '%Y-%m-%dT%H:%M:%S.%fZ')
-    epoch_diff = dt - datetime(1970, 1, 1)
-    return epoch_diff.total_seconds()
+    return dt.timestamp()
 
 def is_relevant(board_name, story):
     if story['type'] != 'system':
@@ -71,8 +75,86 @@ def task_history(board_name, stories):
     history = list(map(history_item, relevant_stories))
     return set_start_column(history, relevant_stories)
 
-def task_dot_count(history):
-    return 4
+def stint(history, now, idx):
+    return {
+        'start': history[idx]['ts'],
+        'end': now if idx == len(history)-1 else history[idx+1]['ts'],
+        'column': history[idx]['column'],
+    }
+
+def weektime(epoch):
+    'Seconds since the beginning of the week'
+    dt = datetime.fromtimestamp(epoch)
+    return (dt.weekday() * 24 * 60 * 60) +\
+        (dt.hour * 60 * 60) +\
+        (dt.minute * 60) +\
+        dt.second +\
+        (dt.microsecond/1000000)
+
+def on_weekend(epoch):
+    'Is this timestamp on the weekend in the current time zone?'
+    return datetime.fromtimestamp(epoch).weekday() > 4
+
+def weekend_end(epoch):
+    'Return the weekend end after this timestamp'
+    dt = datetime.fromtimestamp(epoch)
+    dt += timedelta(days=7-dt.weekday())
+    dt = dt.replace(hour=0, minute=0, second=0, microsecond=0)
+    return dt.timestamp()
+
+def weekend_start(epoch):
+    'Return the weekend start before this timestamp'
+    dt = datetime.fromtimestamp(epoch)
+    days = (dt.weekday() + 2) % 7
+    dt -= timedelta(days=days)
+    dt = dt.replace(hour=0, minute=0, second=0, microsecond=0)
+    return dt.timestamp()
+
+def elapsed(stint):
+    start = stint['start']
+    end = stint['end']
+    total = end - start
+
+    # if start is on a weekend adjust it forward to the end of the weekend
+    # and remove that time from the total
+    if on_weekend(start):
+        delta = min(end, weekend_end(start)) - start
+        total -= delta
+        start += delta
+
+    # if end is on a weekend adjust it back to the start of the weekend
+    # and remove that time from the total
+    if on_weekend(end):
+        delta = end - max(start, weekend_start(end))
+        total -= delta
+        end += delta
+
+    # now that start and end have been adjusted, account for any weekends
+    # in the middle of the range
+    adjusted_total = end - start
+    spanned_weekends = int(adjusted_total/WEEK)
+    if weektime(start) > weektime(end):
+        spanned_weekends += 1
+    total -= 2 * DAY * spanned_weekends
+
+    return total
+
+def task_dot_count(history, now, dot_factor=DAILY_DOTS):
+    """
+       Figure out how long (how many dots) the task has been in its
+       current column.
+       - calculate column stints with start and end for each stint
+       - filter out stints in everything but the current column
+       - calculate elapsed time (omitting weekends) for each stint
+       - sum stints' elapsed time
+    """
+    stints = map(lambda idx: stint(history, now, idx), range(len(history)))
+    filtered_stints = filter(
+        lambda s: s['column'] == history[-1]['column'],
+        stints
+    )
+    total_secs = sum(elapsed(stint) for stint in filtered_stints)
+    return int(total_secs/dot_factor)
 
 class AsanaBoard(board.Board):
     ''' Asana implementation of Board abstract base class. '''
@@ -81,6 +163,7 @@ class AsanaBoard(board.Board):
         self.board_id = board_id
 
     def load(self):
+        now = time.time()
         client = asana.Client.access_token(self.token)
         project = client.projects.find_by_id(self.board_id)
         for task in client.tasks.find_by_project(self.board_id):
@@ -92,5 +175,5 @@ class AsanaBoard(board.Board):
             stories = client.tasks.stories(task['id'])
             history = task_history(project['name'], stories)
             print(history)
-            dot_count = task_dot_count(history)
+            dot_count = task_dot_count(history, now)
             print(dot_count)
--- tests/test_board.py
+++ tests/test_board.py
@@ -1,9 +1,84 @@
+import time
+
 from context import dotty
 from dotty import asana
 
+HOUR = 3600
+DAY = 24 * HOUR
+WEEK = 7 * DAY
+
+MID_SATURDAY = 1494691200 # in EST
+
 class TestDotCount:
-    def test_task_dot_count(self):
-        asana.task_dot_count(None)
+    def test_week_granularity(self):
+        history = [
+            {'ts': MID_SATURDAY - 8*DAY, 'column': 'In Progress'},
+        ]
+        assert 1 == asana.task_dot_count(
+            history, MID_SATURDAY - DAY, asana.WEEKLY_DOTS
+        )
+        history = [
+            {'ts': MID_SATURDAY - ((8*DAY)-HOUR), 'column': 'In Progress'},
+        ]
+        assert 0 == asana.task_dot_count(
+            history, MID_SATURDAY - DAY, asana.WEEKLY_DOTS
+        )
+
+    def test_day_granularity(self):
+        history = [
+            {'ts': MID_SATURDAY - 2*DAY, 'column': 'In Progress'},
+        ]
+        assert 1 == asana.task_dot_count(history, MID_SATURDAY - DAY)
+        history = [
+            {'ts': MID_SATURDAY - (2*DAY - HOUR), 'column': 'In Progress'},
+        ]
+        assert 0 == asana.task_dot_count(history, MID_SATURDAY - DAY)
+
+    def test_within_single_weekend(self):
+        history = [ {'ts': MID_SATURDAY, 'column': 'Foo'} ]
+        assert 0 == asana.task_dot_count(history, MID_SATURDAY+DAY)
+
+    def test_start_on_weekend(self):
+        history = [ {'ts': MID_SATURDAY, 'column': 'Foo'} ]
+        assert 1 == asana.task_dot_count(history, MID_SATURDAY + 3*DAY)
+
+    def test_end_on_weekend(self):
+        history = [ {'ts': MID_SATURDAY - 2*DAY, 'column': 'Foo'} ]
+        assert 1 == asana.task_dot_count(history, MID_SATURDAY)
+
+    def test_spanning_weekend(self):
+        history = [ {'ts': MID_SATURDAY - 2*DAY, 'column': 'Foo'} ]
+        assert 3 == asana.task_dot_count(history, MID_SATURDAY + 3*DAY)
+
+    def test_spanning_two_weekend(self):
+        history = [ {'ts': MID_SATURDAY - 9*DAY, 'column': 'Foo'} ]
+        assert 8 == asana.task_dot_count(history, MID_SATURDAY + 3*DAY)
+
+    def test_start_on_weekend_span_weekend(self):
+        history = [ {'ts': MID_SATURDAY, 'column': 'Foo'} ]
+        assert 6 == asana.task_dot_count(history, MID_SATURDAY + 10*DAY)
+
+    def test_span_weekend_end_on_weekend(self):
+        history = [ {'ts': MID_SATURDAY - 8*DAY, 'column': 'Foo'} ]
+        assert 5 == asana.task_dot_count(history, MID_SATURDAY)
+
+    def test_start_on_weekend_span_weekend_end_on_weekend(self):
+        history = [ {'ts': MID_SATURDAY - 14*DAY, 'column': 'Foo'} ]
+        assert 10 == asana.task_dot_count(history, MID_SATURDAY)
+
+    def test_multiple_column_stints(self):
+        history = [
+            {'ts': MID_SATURDAY - 4*DAY, 'column': 'In Progress'},
+            {'ts': MID_SATURDAY - 3*DAY, 'column': 'Code Review'},
+            {'ts': MID_SATURDAY - 2*DAY, 'column': 'In Progress'},
+        ]
+        assert 2 == asana.task_dot_count(history, MID_SATURDAY-DAY)
+
+    def test_single_history_item(self):
+        history = [
+            {'ts': MID_SATURDAY - 3*DAY, 'column': None},
+        ]
+        assert 2 == asana.task_dot_count(history, MID_SATURDAY-DAY)
 
 class TestHistory:
     def test_creation_only(self):
@@ -16,7 +91,7 @@ class TestHistory:
             },
         ]
         expected_history = [
-            { 'ts': 1494687691.287, 'column': None },
+            { 'ts': 1494702091.287, 'column': None },
         ]
         assert asana.task_history(board_name, stories) == expected_history
 
@@ -35,8 +110,8 @@ class TestHistory:
             },
         ]
         expected_history = [
-            { 'ts': 1494687691.287, 'column': 'Ready' },
-            { 'ts': 1494687737.502, 'column': 'Develop (Max WIP: 1)' },
+            { 'ts': 1494702091.287, 'column': 'Ready' },
+            { 'ts': 1494702137.502, 'column': 'Develop (Max WIP: 1)' },
         ]
         assert asana.task_history(board_name, stories) == expected_history
 
@@ -55,8 +130,8 @@ class TestHistory:
             },
         ]
         expected_history = [
-            { 'ts': 1494687691.287, 'column': 'Ready' },
-            { 'ts': 1494687737.502, 'column': 'Develop (Max WIP: 1)' },
+            { 'ts': 1494702091.287, 'column': 'Ready' },
+            { 'ts': 1494702137.502, 'column': 'Develop (Max WIP: 1)' },
         ]
         assert asana.task_history(board_name, stories) == expected_history
 
@@ -97,7 +172,7 @@ class TestHistory:
             },
         ]
         expected_history = [
-            { 'ts': 1494687691.287, 'column': 'Ready' },
-            { 'ts': 1494687737.502, 'column': 'Develop (Max WIP: 1)' },
+            { 'ts': 1494702091.287, 'column': 'Ready' },
+            { 'ts': 1494702137.502, 'column': 'Develop (Max WIP: 1)' },
         ]
         assert asana.task_history(board_name, stories) == expected_history

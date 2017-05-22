22c0f911b65a2bf467ee31e2315d9522717d422e
ctcutler <ctcutler@gmail.com>
2017-05-21 22:04:50 -0400

    split functional code into helpers.py

--- dotty/board.py
+++ dotty/board.py
@@ -1,165 +1,20 @@
 # -*- coding: UTF-8 -*-
 
-from datetime import datetime, timedelta
-import json
-import re
 import time
+import re
 
 import asana
+
+from dotty import helpers
 
 DOT = '•'
 # dots show up either as 'foo bar | •••' or as 'foo bar | • x 12'
 DOTS_RE = re.compile(r'^.+ \| ('+DOT+'+|'+DOT+' x \d+)$')
-ADD_RE = re.compile(r'^added to (?P<board>.+)$')
-MOVE_RE = re.compile(
-    r'^moved from (?P<from_column>.+) to (?P<column>.+) \((?P<board>.+)\)$')
 DAY = 24 * 60 * 60
 WEEK = 7 * DAY
 WEEKLY_DOTS = 5 * DAY
 DAILY_DOTS = DAY
 MAX_DOTS = 5
-
-def iso8601_to_epoch(iso_string):
-    dt = datetime.strptime(iso_string, '%Y-%m-%dT%H:%M:%S.%fZ')
-    return dt.timestamp()
-
-def is_relevant(board_name, story):
-    if story['type'] != 'system':
-        return False
-
-    match = ADD_RE.search(story['text']) or MOVE_RE.search(story['text'])
-
-    if not match:
-        return False
-
-    board = match.groupdict().get('board')
-
-    if board != board_name:
-        return False
-
-    return True
-
-def history_item(story):
-    match = ADD_RE.search(story['text']) or MOVE_RE.search(story['text'])
-    column = match.groupdict().get('column')
-    ts = iso8601_to_epoch(story['created_at'])
-
-    return { 'ts': ts, 'column': column }
-
-def set_start_column(history, stories):
-    '''
-    The first story doesn't mention a column so we have to update the first
-    item in the history based on the *from* column of the second story
-    (the first move).
-    '''
-    if len(stories) < 2:
-        return history
-
-    story = stories[1]
-    match = MOVE_RE.search(story['text'])
-
-    if not match:
-        return history
-
-    column = match.groupdict().get('from_column')
-    new_item = history[0].copy()
-    new_item['column'] = column
-
-    return [new_item] + history[1:]
-
-def task_history(board_name, stories):
-    '''
-    Converts a list of task stories to a history of that task.
-
-    See unit tests for examples.
-    '''
-    sorted_stories = sorted(stories, key=lambda s: s['created_at'])
-    relevant_stories = list(
-        filter(lambda s: is_relevant(board_name, s), sorted_stories)
-    )
-    history = list(map(history_item, relevant_stories))
-    return set_start_column(history, relevant_stories)
-
-def stint(history, now, idx):
-    return {
-        'start': history[idx]['ts'],
-        'end': now if idx == len(history)-1 else history[idx+1]['ts'],
-        'column': history[idx]['column'],
-    }
-
-def weektime(epoch):
-    'Seconds since the beginning of the week'
-    dt = datetime.fromtimestamp(epoch)
-    return (dt.weekday() * 24 * 60 * 60) +\
-        (dt.hour * 60 * 60) +\
-        (dt.minute * 60) +\
-        dt.second +\
-        (dt.microsecond/1000000)
-
-def on_weekend(epoch):
-    'Is this timestamp on the weekend in the current time zone?'
-    return datetime.fromtimestamp(epoch).weekday() > 4
-
-def weekend_end(epoch):
-    'Return the weekend end after this timestamp'
-    dt = datetime.fromtimestamp(epoch)
-    dt += timedelta(days=7-dt.weekday())
-    dt = dt.replace(hour=0, minute=0, second=0, microsecond=0)
-    return dt.timestamp()
-
-def weekend_start(epoch):
-    'Return the weekend start before this timestamp'
-    dt = datetime.fromtimestamp(epoch)
-    days = (dt.weekday() + 2) % 7
-    dt -= timedelta(days=days)
-    dt = dt.replace(hour=0, minute=0, second=0, microsecond=0)
-    return dt.timestamp()
-
-def elapsed(stint):
-    start = stint['start']
-    end = stint['end']
-    total = end - start
-
-    # if start is on a weekend adjust it forward to the end of the weekend
-    # and remove that time from the total
-    if on_weekend(start):
-        delta = min(end, weekend_end(start)) - start
-        total -= delta
-        start += delta
-
-    # if end is on a weekend adjust it back to the start of the weekend
-    # and remove that time from the total
-    if on_weekend(end):
-        delta = end - max(start, weekend_start(end))
-        total -= delta
-        end += delta
-
-    # now that start and end have been adjusted, account for any weekends
-    # in the middle of the range
-    adjusted_total = end - start
-    spanned_weekends = int(adjusted_total/WEEK)
-    if weektime(start) > weektime(end):
-        spanned_weekends += 1
-    total -= 2 * DAY * spanned_weekends
-
-    return total
-
-def task_dot_count(history, now, dot_factor=DAILY_DOTS):
-    """
-       Figure out how long (how many dots) the task has been in its
-       current column.
-       - calculate column stints with start and end for each stint
-       - filter out stints in everything but the current column
-       - calculate elapsed time (omitting weekends) for each stint
-       - sum stints' elapsed time
-    """
-    stints = map(lambda idx: stint(history, now, idx), range(len(history)))
-    filtered_stints = filter(
-        lambda s: s['column'] == history[-1]['column'],
-        stints
-    )
-    total_secs = sum(elapsed(stint) for stint in filtered_stints)
-    return int(total_secs/dot_factor)
 
 class Board:
     ''' Asana implementation of Board abstract base class. '''
@@ -193,9 +48,9 @@ class Board:
                 continue
 
             stories = client.tasks.stories(task_id)
-            history = task_history(project_name, stories)
+            history = helpers.task_history(project_name, stories)
             dot_factor = WEEKLY_DOTS if self.weekly_dots else DAILY_DOTS
-            dot_count = task_dot_count(history, now, dot_factor=dot_factor)
+            dot_count = helpers.task_dot_count(history, now, dot_factor)
 
             if DOTS_RE.search(task_name):
                 new_task_name = task_name.rsplit(' | ', 1)[0]
--- tests/test_board.py
+++ tests/test_board.py
@@ -1,11 +1,13 @@
 import time
 
 from context import dotty
-from dotty import board
+from dotty import helpers
 
 HOUR = 3600
 DAY = 24 * HOUR
 WEEK = 7 * DAY
+WEEKLY_DOTS = 5 * DAY
+DAILY_DOTS = DAY
 
 MID_SATURDAY = 1494691200 # in EST
 
@@ -14,57 +16,65 @@ class TestDotCount:
         history = [
             {'ts': MID_SATURDAY - 8*DAY, 'column': 'In Progress'},
         ]
-        assert 1 == board.task_dot_count(
-            history, MID_SATURDAY - DAY, board.WEEKLY_DOTS
-        )
+        assert 1 == helpers.task_dot_count(
+            history, MID_SATURDAY - DAY, WEEKLY_DOTS)
         history = [
             {'ts': MID_SATURDAY - ((8*DAY)-HOUR), 'column': 'In Progress'},
         ]
-        assert 0 == board.task_dot_count(
-            history, MID_SATURDAY - DAY, board.WEEKLY_DOTS
-        )
+        assert 0 == helpers.task_dot_count(
+            history, MID_SATURDAY - DAY, WEEKLY_DOTS)
 
     def test_day_granularity(self):
         history = [
             {'ts': MID_SATURDAY - 2*DAY, 'column': 'In Progress'},
         ]
-        assert 1 == board.task_dot_count(history, MID_SATURDAY - DAY)
+        assert 1 == helpers.task_dot_count(
+            history, MID_SATURDAY - DAY, DAILY_DOTS)
         history = [
             {'ts': MID_SATURDAY - (2*DAY - HOUR), 'column': 'In Progress'},
         ]
-        assert 0 == board.task_dot_count(history, MID_SATURDAY - DAY)
+        assert 0 == helpers.task_dot_count(
+            history, MID_SATURDAY - DAY, DAILY_DOTS)
 
     def test_within_single_weekend(self):
         history = [ {'ts': MID_SATURDAY, 'column': 'Foo'} ]
-        assert 0 == board.task_dot_count(history, MID_SATURDAY+DAY)
+        assert 0 == helpers.task_dot_count(
+            history, MID_SATURDAY+DAY, DAILY_DOTS)
 
     def test_start_on_weekend(self):
         history = [ {'ts': MID_SATURDAY, 'column': 'Foo'} ]
-        assert 1 == board.task_dot_count(history, MID_SATURDAY + 3*DAY)
+        assert 1 == helpers.task_dot_count(
+            history, MID_SATURDAY + 3*DAY, DAILY_DOTS)
 
     def test_end_on_weekend(self):
         history = [ {'ts': MID_SATURDAY - 2*DAY, 'column': 'Foo'} ]
-        assert 1 == board.task_dot_count(history, MID_SATURDAY)
+        assert 1 == helpers.task_dot_count(
+            history, MID_SATURDAY, DAILY_DOTS)
 
     def test_spanning_weekend(self):
         history = [ {'ts': MID_SATURDAY - 2*DAY, 'column': 'Foo'} ]
-        assert 3 == board.task_dot_count(history, MID_SATURDAY + 3*DAY)
+        assert 3 == helpers.task_dot_count(
+            history, MID_SATURDAY + 3*DAY, DAILY_DOTS)
 
     def test_spanning_two_weekend(self):
         history = [ {'ts': MID_SATURDAY - 9*DAY, 'column': 'Foo'} ]
-        assert 8 == board.task_dot_count(history, MID_SATURDAY + 3*DAY)
+        assert 8 == helpers.task_dot_count(
+            history, MID_SATURDAY + 3*DAY, DAILY_DOTS)
 
     def test_start_on_weekend_span_weekend(self):
         history = [ {'ts': MID_SATURDAY, 'column': 'Foo'} ]
-        assert 6 == board.task_dot_count(history, MID_SATURDAY + 10*DAY)
+        assert 6 == helpers.task_dot_count(
+            history, MID_SATURDAY + 10*DAY, DAILY_DOTS)
 
     def test_span_weekend_end_on_weekend(self):
         history = [ {'ts': MID_SATURDAY - 8*DAY, 'column': 'Foo'} ]
-        assert 5 == board.task_dot_count(history, MID_SATURDAY)
+        assert 5 == helpers.task_dot_count(
+            history, MID_SATURDAY, DAILY_DOTS)
 
     def test_start_on_weekend_span_weekend_end_on_weekend(self):
         history = [ {'ts': MID_SATURDAY - 14*DAY, 'column': 'Foo'} ]
-        assert 10 == board.task_dot_count(history, MID_SATURDAY)
+        assert 10 == helpers.task_dot_count(
+           history, MID_SATURDAY, DAILY_DOTS)
 
     def test_multiple_column_stints(self):
         history = [
@@ -72,13 +82,15 @@ class TestDotCount:
             {'ts': MID_SATURDAY - 3*DAY, 'column': 'Code Review'},
             {'ts': MID_SATURDAY - 2*DAY, 'column': 'In Progress'},
         ]
-        assert 2 == board.task_dot_count(history, MID_SATURDAY-DAY)
+        assert 2 == helpers.task_dot_count(
+            history, MID_SATURDAY-DAY, DAILY_DOTS)
 
     def test_single_history_item(self):
         history = [
             {'ts': MID_SATURDAY - 3*DAY, 'column': None},
         ]
-        assert 2 == board.task_dot_count(history, MID_SATURDAY-DAY)
+        assert 2 == helpers.task_dot_count(
+            history, MID_SATURDAY-DAY, DAILY_DOTS)
 
 class TestHistory:
     def test_creation_only(self):
@@ -93,7 +105,7 @@ class TestHistory:
         expected_history = [
             { 'ts': 1494702091.287, 'column': None },
         ]
-        assert board.task_history(board_name, stories) == expected_history
+        assert helpers.task_history(board_name, stories) == expected_history
 
     def test_creation_and_move(self):
         board_name = 'Kanban Example II'
@@ -113,7 +125,7 @@ class TestHistory:
             { 'ts': 1494702091.287, 'column': 'Ready' },
             { 'ts': 1494702137.502, 'column': 'Develop (Max WIP: 1)' },
         ]
-        assert board.task_history(board_name, stories) == expected_history
+        assert helpers.task_history(board_name, stories) == expected_history
 
     def test_creation_after_move(self):
         board_name = 'Kanban Example II'
@@ -133,7 +145,7 @@ class TestHistory:
             { 'ts': 1494702091.287, 'column': 'Ready' },
             { 'ts': 1494702137.502, 'column': 'Develop (Max WIP: 1)' },
         ]
-        assert board.task_history(board_name, stories) == expected_history
+        assert helpers.task_history(board_name, stories) == expected_history
 
     def test_ignore_unrelated_projects(self):
         board_name = 'Kanban Example II'
@@ -150,7 +162,7 @@ class TestHistory:
             },
         ]
         expected_history = []
-        assert board.task_history(board_name, stories) == expected_history
+        assert helpers.task_history(board_name, stories) == expected_history
 
     def test_ignore_unrelated_stories(self):
         board_name = 'Kanban Example II'
@@ -175,4 +187,4 @@ class TestHistory:
             { 'ts': 1494702091.287, 'column': 'Ready' },
             { 'ts': 1494702137.502, 'column': 'Develop (Max WIP: 1)' },
         ]
-        assert board.task_history(board_name, stories) == expected_history
+        assert helpers.task_history(board_name, stories) == expected_history
